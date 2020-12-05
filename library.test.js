0221d7f8ac034e9f4d89ad31d954b5bdb3903b95
Patric Kangasmäki <patric.kangasmaki@tuni.fi>
2020-12-05 22:34:33 +0200

    tests for toString

--- library.test.js
+++ library.test.js
@@ -7,6 +7,7 @@ import castArray from './src/castArray.js';
 //Patric importit
 import words from './src/words'
 import upperFirst from './src/upperFirst.js';
+import toString from './src/toString'
 
 describe("Tests for 'add' function", () => {
 
@@ -156,7 +157,37 @@ PATRIC
 
 */
 
+
+
 describe("pkangasmaki", () => {
+    describe("Tests for 'toString' function", () => {
+        test('Object converted to string', () => {
+            const testObject = {
+                username: 'username',
+                name: 'name'
+            }
+            //tests if the type of testObject has changed to string after running through toString function
+            expect(typeof(toString(testObject))).toBe('string')
+        })
+        test('Number converted to string', () => {
+            expect(typeof(toString(1.1))).toBe('string')
+        })
+        test('String converted to string', () => {
+            expect(typeof(toString('Test'))).toBe('string')
+        })
+        test('Array converted to string', () => {
+            const testArray = ['Hello', 'World']
+            //tests if the type of testArray has changed to string after running through toString function
+            expect(typeof(toString(testArray))).toBe('string')
+        })
+        test('JSON converted to string', () => {
+            const date = new Date();
+            const testJSON = date.toJSON();
+            //tests if the type of testObject has changed to string after running through toString function
+            expect(typeof(toString(testJSON))).toBe('string')
+        })
+    })
+    
     describe("Tests for 'forUpperFirst' function", () => {
         test('String starting with lowercase letter', () => {
             expect(upperFirst('laadukas')).toEqual('Laadukas')
